dd8f5cb82e2ba1e2bb7935ca9f403884adf4d47a
analog-hors <44050761+analog-hors@users.noreply.github.com>
2025-06-30 13:22:57 +1000

    Implement data-dependent initialization for ActNorm

--- glow_tts/decoder/act_norm.py
+++ glow_tts/decoder/act_norm.py
@@ -3,15 +3,17 @@ import torch, torch.nn as nn, torch.nn.functional as F
 class ActNorm(nn.Module):
     in_channels: int
 
-    scale: torch.Tensor
+    log_scale: torch.Tensor
     shift: torch.Tensor
+    initialized: torch.Tensor
 
     def __init__(self, in_channels: int):
         super().__init__()
         self.in_channels = in_channels
 
-        self.scale = nn.Parameter(torch.zeros((1, in_channels, 1)))
+        self.log_scale = nn.Parameter(torch.zeros((1, in_channels, 1)))
         self.shift = nn.Parameter(torch.zeros((1, in_channels, 1)))
+        self.register_buffer("initialized", torch.tensor(False), persistent=True)
 
     def forward(
         self,
@@ -29,16 +31,41 @@ class ActNorm(nn.Module):
             - logdet: `(batch)`
         """
 
+        if not self.initialized.item():
+            with torch.no_grad():
+                # Compute per-channel mean, variance, and log stdev.
+                # total: ()
+                # mean: (in_channels)
+                # mean_sq: (in_channels)
+                # variance: (in_channels)
+                # log_stdev: (in_channels)
+                total = torch.sum(mask)
+                mean = torch.sum(seq * mask, (0, 2)) / total
+                mean_sq = torch.sum(seq * seq * mask, (0, 2)) / total
+                variance = mean_sq - mean * mean
+                log_stdev = 0.5 * torch.log(torch.clamp_min(variance, 1e-6))
+
+                # Compute per-channel initializations for shift and log scale.
+                # shift_init: (in_channels)
+                # log_scale_init: (in_channels)
+                shift_init = -mean * torch.exp(-log_stdev)
+                log_scale_init = -log_stdev
+
+                # Initialize parameters.
+                self.shift.copy_(shift_init.unsqueeze(0).unsqueeze(2))
+                self.log_scale.copy_(log_scale_init.unsqueeze(0).unsqueeze(2))
+                self.initialized.fill_(True)
+
         # Scale, shift, and mask.
         # Also compute the logdet of the Jacobian if going forwards.
         # seq: (batch, in_channels, seq_len)
         # logdet: (batch)
         if not reverse:
             lengths = torch.sum(mask, (1, 2))
-            seq = (torch.exp(self.scale) * seq + self.shift) * mask
-            logdet = torch.sum(self.scale) * lengths
+            seq = (torch.exp(self.log_scale) * seq + self.shift) * mask
+            logdet = torch.sum(self.log_scale) * lengths
         else:
-            seq = (seq - self.shift) * torch.exp(-self.scale) * mask
+            seq = (seq - self.shift) * torch.exp(-self.log_scale) * mask
             logdet = None
 
         return seq, logdet
